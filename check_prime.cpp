68087d20e8f7f88b4f5c1e88697c14c785aeca71
Nilesh Tiwari <93751356+Niltiwari7@users.noreply.github.com>
2023-09-26 04:09:56 +0530

    Update check_prime.cpp

--- check_prime.cpp
+++ check_prime.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 
-bool isprime(int n){
-    int cnt=0;
-    for(int i=2;i<=n;i++){
-        if(n%i==0)cnt++;
-        if(cnt>1)return false;
+
+bool check(int n){
+    if(n==1)return false;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0)return false;
     }
     return true;
 }
