d28635037a354175f9cd3e83b807405964cf48c0
Nilesh Tiwari <93751356+Niltiwari7@users.noreply.github.com>
2024-04-05 07:09:55 +0530

    Update print_all_divisior.cpp

--- Math/print_all_divisior.cpp
+++ Math/print_all_divisior.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 
+void print_divisors(int n) {
+        // Code here.
+        vector<int>ans;
+        for(int i=1;i<=sqrt(n);i++){
+            if(n%i==0){
+                ans.push_back(i);
+                if(n/i!=i)ans.push_back(n/i);
+            }
+        }
+        sort(begin(ans),end(ans));
+        for(auto it:ans)cout<<it<<" ";
+}
+
 int main(){
     int n;
     cin>>n;
@@ -8,3 +21,5 @@ int main(){
         if(n%i==0) cout<<i<<endl;
     }
 }
+
+
