dcfefb4a540df7d0787593b8aeb5acb3d5b75415
Nilesh Tiwari <93751356+Niltiwari7@users.noreply.github.com>
2024-06-29 18:21:13 +0530

    Update leetcodeTemplate.cpp

--- leetcodeTemplate.cpp
+++ leetcodeTemplate.cpp
@@ -5,7 +5,7 @@ typedef pair<int, int> pii;
 typedef unordered_map<int, int> uimap;
 typedef unordered_map<char, int> ucmap;
 typedef priority_queue<int, vector<int>, greater<int>> min_pq; // Min-heap
-typedef priority_queue<int> max_pq; // Max-heap
+typedef priority_queue<int> max_pq;                            // Max-heap
 
 #define fo(i, n) for (int i = 0; i < n; i++)
 #define Fo(i, k, n) for (int i = k; i < n; i++)
@@ -17,12 +17,14 @@ typedef priority_queue<int> max_pq; // Max-heap
 #define se second
 #define sz(x) (int)x.size()
 #define endl '\n'
-#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define fast_io()                                                              \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(NULL)
 #define precision(x) cout << fixed << setprecision(x)
 #define bitcount(x) __builtin_popcount(x) // Number of set bits in an integer
-#define gcd(a, b) __gcd(a, b) // Greatest Common Divisor
-#define lcm(a, b) (a * (b / gcd(a, b))) // Least Common Multiple
-#define sort_asc(v) sort(all(v)) // Sort in ascending order
+#define gcd(a, b) __gcd(a, b)             // Greatest Common Divisor
+#define lcm(a, b) (a * (b / gcd(a, b)))   // Least Common Multiple
+#define sort_asc(v) sort(all(v))          // Sort in ascending order
 #define sort_desc(v) sort(all(v), greater<int>()) // Sort in descending order
 
 const int INF = 1e9;
@@ -30,7 +32,7 @@ const ll LINF = 1e18;
 const int MOD = 1e9 + 7;
 int drow[4] = {-1, 1, 0, 0};
 int dcol[4] = {0, 0, -1, 1};
-int dr[8] = {-1, -1, -1, 0, 1, 1, 1, 0}; 
+int dr[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
 int dc[8] = {-1, 0, 1, -1, -1, 0, 1, 1};
 
 template <typename T> void debug_1d(const vector<T>& v) {
@@ -49,7 +51,8 @@ template <typename T> void debug_2d(const vector<vector<T>>& v) {
     }
 }
 
-template <typename T1, typename T2> void debug_map(const unordered_map<T1, T2>& m) {
+template <typename T1, typename T2>
+void debug_map(const unordered_map<T1, T2>& m) {
     for (const auto& p : m) {
         cout << p.first << " : " << p.second << endl;
     }
