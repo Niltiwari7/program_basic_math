1ed9f7da7712a3e95776b2cb61547523681bc941
Niltiwari7 <nt465638@gmail.com>
2024-07-11 09:31:00 +0530

    updated dsu

--- Graph_Alogrithm/Dsu.cpp
+++ Graph_Alogrithm/Dsu.cpp
@@ -2,36 +2,56 @@ class DSU {
 public:
     vector<int> parent;
     vector<int> rank;
+    vector<int> size;
 
-    DSU(int size) {
-        parent.resize(size);
-        rank.resize(size, 0);
-        for (int i = 0; i < size; ++i) {
+    // Constructor to initialize the DSU
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        size.resize(n, 1);
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
     }
 
-    int find(int i) {
-        if (i == parent[i]) {
-            return i;
+    // Find the parent of a node with path compression
+    int find_parent(int x) {
+        if (parent[x] == x) {
+            return parent[x];
         }
-        return parent[i] = find(parent[i]);
+        return parent[x] = find_parent(parent[x]);
     }
 
+    // Union by rank
+    void Union(int x, int y) {
+        int parent_x = find_parent(x);
+        int parent_y = find_parent(y);
 
-    void unite(int x, int y) {
-        int x_parent = find(x);
-        int y_parent = find(y);
-        if (x_parent == y_parent) {
-            return;
-        }
-        if (rank[x_parent] > rank[y_parent]) {
-            parent[y_parent] = x_parent;
-        } else if (rank[x_parent] < rank[y_parent]) {
-            parent[x_parent] = y_parent;
+        if (parent_x == parent_y) return;
+
+        if (rank[parent_x] < rank[parent_y]) {
+            parent[parent_x] = parent_y;
+        } else if (rank[parent_x] > rank[parent_y]) {
+            parent[parent_y] = parent_x;
         } else {
-            parent[x_parent] = y_parent;
-            rank[y_parent]++;
+            parent[parent_y] = parent_x;
+            rank[parent_x]++;
+        }
+    }
+
+    // Union by size
+    void UnionBySize(int x, int y) {
+        int parent_x = find_parent(x);
+        int parent_y = find_parent(y);
+
+        if (parent_x == parent_y) return;
+
+        if (size[parent_x] < size[parent_y]) {
+            parent[parent_x] = parent_y;
+            size[parent_y] += size[parent_x];
+        } else {
+            parent[parent_y] = parent_x;
+            size[parent_x] += size[parent_y];
         }
     }
 };
